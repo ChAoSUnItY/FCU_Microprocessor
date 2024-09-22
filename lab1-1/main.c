0354a7acb04eb16c1d60fcb25c7475b1dd4747e5
Kyle Lin <minecraft.kyle.train@gmail.com>
2024-09-22 18:19:38 +0800

    Initialize repository

---
+++ lab1-1/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "NUC100Series.h"
+#include "MCU_init.h"
+#include "SYS_init.h"
+#include "Scankey.h"
+
+const char number[7] = "1109998";
+volatile uint32_t* leds[4] = {&PC12, &PC13, &PC14, &PC15};
+
+void UpdateAllLEDs(uint32_t toggle) {
+	for (uint32_t i = 0; i < 4; i++)
+		*(leds[i]) = !toggle;
+}
+
+void UpdateLEDs(uint32_t pos) {
+	for (uint32_t i = 0; i < 4; i++)
+		*(leds[i]) = pos == i ? 0 : 1;
+}
+
+void Init_GPIO(void) {
+	GPIO_SetMode(PC, BIT12, GPIO_MODE_OUTPUT);
+	GPIO_SetMode(PC, BIT13, GPIO_MODE_OUTPUT);
+	GPIO_SetMode(PC, BIT14, GPIO_MODE_OUTPUT);
+	GPIO_SetMode(PC, BIT15, GPIO_MODE_OUTPUT);
+	UpdateAllLEDs(0);
+}
+
+void SetLEDs(char digit) {
+	for (uint32_t i = 0; i < 4; i++)
+		*(leds[i]) = ~((digit >> i) & 1);
+}
+
+int main(void)
+{
+	uint32_t i;
+  SYS_Init();
+	OpenKeyPad();
+	Init_GPIO();
+		
+	while (1) {
+		i = ScanKey();
+		if (i >= 1 && i <= 7) {
+			SetLEDs(number[i - 1]);
+		} else {
+			UpdateAllLEDs(0);
+		}
+	}
+}
---
+++ lab1-2/main.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include "NUC100Series.h"
+#include "MCU_init.h"
+#include "SYS_init.h"
+#include "Scankey.h"
+
+volatile uint32_t* leds[4] = {&PC12, &PC13, &PC14, &PC15};
+
+void UpdateAllLEDs(uint32_t toggle) {
+	for (uint32_t i = 0; i < 4; i++)
+		*(leds[i]) = !toggle;
+}
+
+void UpdateLEDs(uint32_t pos) {
+	for (uint32_t i = 0; i < 4; i++)
+		*(leds[i]) = pos == i ? 0 : 1;
+}
+
+void Init_GPIO(void) {
+	GPIO_SetMode(PC, BIT12, GPIO_MODE_OUTPUT);
+	GPIO_SetMode(PC, BIT13, GPIO_MODE_OUTPUT);
+	GPIO_SetMode(PC, BIT14, GPIO_MODE_OUTPUT);
+	GPIO_SetMode(PC, BIT15, GPIO_MODE_OUTPUT);
+	UpdateAllLEDs(0);
+}
+
+typedef enum {
+	S_Uninitialized,
+	S_L2R,
+	S_Flash,
+	S_R2L
+} State;
+
+int main(void)
+{
+	State state = S_Uninitialized;
+	uint32_t i, pos;
+  SYS_Init();
+	OpenKeyPad();
+	Init_GPIO();
+	
+	while (1) {
+		i = ScanKey();
+		
+		if (i != state) {
+			switch (i) {
+				case 1: {
+					state = S_L2R;
+					UpdateLEDs(pos = 0);
+					CLK_SysTickDelay(100000);
+					break;
+				}
+				case 2: {
+					state = S_Flash;
+					UpdateAllLEDs(pos = 1);
+					CLK_SysTickDelay(100000);
+					break;
+				}
+				case 3: {
+					state = S_R2L;
+					UpdateLEDs(pos = 3);
+					CLK_SysTickDelay(100000);
+					break;
+				}
+				default: {
+					state = S_Uninitialized;
+					UpdateAllLEDs(0);
+					break;
+				}
+			}
+		}
+		
+		switch (i) {
+			case 1: {
+				UpdateLEDs(pos = (pos + 1) % 4);
+				CLK_SysTickDelay(100000);
+				break;
+			}
+			case 2: {
+				UpdateAllLEDs(pos ^= 1);
+				CLK_SysTickDelay(100000);
+				break;
+			}
+			case 3: {
+				UpdateLEDs(pos = (pos ? pos - 1 : 3));
+				CLK_SysTickDelay(100000);
+				break;
+			}
+			default: break;
+		}
+	}
+}
